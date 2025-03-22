cc2139f65522bdfb3b66b8fe8a403fcd23edcaf1
roshi <srivastavaroshi00@gmail.com>
2025-03-22 15:22:45 +0530

    new tab on clicking logo

--- angular-Project/src/app/app.component.ts
+++ angular-Project/src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Helloworld";
-  imgUrl = "./assets/bridgelabzlogo.webp";
+  imgUrl = "../assets/bridgelabzlogo.webp";
+  url = "https://www.bridgelabz.com/";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz!!!";
+  }
+
+  onClick($event: Event) {
+    console.log("Save button is Clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
